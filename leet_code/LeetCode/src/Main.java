709565e688889545da04a3bc137c0f831fc07c18
Muhammad M. Mostey <muhammadmostey@gmail.com>
2023-11-14 13:34:21 +0300

    Update Main.java

--- leet_code/LeetCode/src/Main.java
+++ leet_code/LeetCode/src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int[] array = new int[2];
-        array[0] = 3;
-        array[1] = 3;
-        System.out.println( TwoSum.twoSum(array, 6) );
+      int steps = 10;
+      for (steps; steps--;){
+          System.out.println(steps);
+      }
     }
 
 }
