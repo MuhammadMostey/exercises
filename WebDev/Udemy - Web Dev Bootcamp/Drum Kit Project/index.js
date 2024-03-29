f3693436ba27eab1bfcbe73632b66fe43b0d76af
Muhammad M. Mostey <muhammadmostey@gmail.com>
2024-03-29 14:23:20 +0300

    adds logic of playing sounds for each drum

--- WebDev/Udemy - Web Dev Bootcamp/Drum Kit Project/index.js
+++ WebDev/Udemy - Web Dev Bootcamp/Drum Kit Project/index.js
@@ -1,1 +1,44 @@
+let drumButtonElement = document.querySelectorAll(".drum");
 
+let crashSound = new Audio("sounds/crash.mp3");
+let tomOneSound = new Audio("sounds/tom-1.mp3");
+let tomTwoSound = new Audio("sounds/tom-2.mp3");
+let tomThreeSound = new Audio("sounds/tom-3.mp3");
+let tomFourSound = new Audio("sounds/tom-4.mp3");
+let kickDrumSound = new Audio("sounds/kick-bass.mp3");
+let snareSound = new Audio("sounds/snare.mp3");
+
+for (let i = 0; i < drumButtonElement.length; i++) {
+  drumButtonElement[i].addEventListener("click", function () {
+    switch (this.innerHTML) {
+      case "w":
+        tomOneSound.play();
+        break;
+      case "a":
+        tomTwoSound.play();
+        break;
+      case "s":
+        tomThreeSound.play();
+        break;
+      case "d":
+        tomFourSound.play();
+        break;
+      case "j":
+        snareSound.play();
+        break;
+      case "k":
+        crashSound.play();
+        break;
+      case "l":
+        kickDrumSound.play();
+        break;
+      default:
+        "";
+        break;
+    }
+  });
+}
+
+// function handleClick() {
+//   audio1.play();
+// }
